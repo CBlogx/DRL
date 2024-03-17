c068a68835f853ac485fa82db6e0c2b16a990082
CBlogx <958809247@qq.com>
2024-03-17 15:57:40 +0800

    Change fileName and Create a CNN env template

--- Stable baselines3/Example/2-DQN.py
+++ Stable baselines3/Example/2-DQN.py
@@ -8,34 +8,35 @@ from stable_baselines3.common.evaluation import evaluate_policy
 env = gym.make("LunarLander-v2", render_mode="rgb_array")
 
 # 2. Instantiate the agent
-model = DQN("MlpPolicy", env, verbose=1, device="cuda")
+model = DQN("MlpPolicy", env, verbose=1)
+# model = DQN("MlpPolicy", env, verbose=1, device="cuda")
 
 # 3. Train the agent and display a progress bar
 model.learn(total_timesteps=int(2e5), progress_bar=True)
 
-# 4. Save the agent
-model.save("dqn_lunar")
+# # 4. Save the agent
+# model.save("dqn_lunar")
 
-# 5. Delete the trained model to demonstrate loading
-del model
+# # 5. Delete the trained model to demonstrate loading
+# del model
 
-# 6. Load the trained model
-model = DQN.load("dqn_lunar", env=env)
+# # 6. Load the trained model
+# model = DQN.load("dqn_lunar", env=env)
 
-# 7. Evaluate the agent
-# When use wrappers with the environment that modify rewards,
-# this will be reflected here. To evaluate with original rewards,
-# wrap environment in a "Monitor" wrapper before other wrappers.
-mean_reward, std_reward = evaluate_policy(
-    model, model.get_env(), n_eval_episodes=10)
+# # 7. Evaluate the agent
+# # When use wrappers with the environment that modify rewards,
+# # this will be reflected here. To evaluate with original rewards,
+# # wrap environment in a "Monitor" wrapper before other wrappers.
+# mean_reward, std_reward = evaluate_policy(
+#     model, model.get_env(), n_eval_episodes=10)
 
-# 8. Train model usage
-vec_env = model.get_env()
-obs = vec_env.reset()
-for i in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, rewards, dones, info = vec_env.step(action)
-    vec_env.render("human")
+# # 8. Train model usage
+# vec_env = model.get_env()
+# obs = vec_env.reset()
+# for i in range(1000):
+#     action, _states = model.predict(obs, deterministic=True)
+#     obs, rewards, dones, info = vec_env.step(action)
+#     vec_env.render("human")
 
 # 输出信息
 # -----------------------------------------
--- Stable baselines3/test.py
+++ Stable baselines3/test.py
@@ -3,3 +3,10 @@ if torch.cuda.is_available():
     print("Using GPU")
 else:
     print("Using CPU")
+# Check for available GPUs
+num_gpus = torch.cuda.device_count()
+print("Number of available GPUs:", num_gpus)
+
+# List all available GPUs
+for i in range(num_gpus):
+    print("GPU device", i, ":", torch.cuda.get_device_name(i))
