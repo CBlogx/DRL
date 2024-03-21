3195a5eb964af9a545603dae34a1cb27402719ea
CBlogx <958809247@qq.com>
2024-03-21 10:10:30 +0800

    change code

--- Code/Data_Processing.js
+++ Code/Data_Processing.js
@@ -1,10 +1,17 @@
 const fs = require("fs");
+const current_date = "26/02/2024";
 
+const date_convert = (date) => {
+  let parts = date.split("/");
+  let newDate = parts[2] + "-" + parts[1] + "-" + parts[0];
+  return newDate;
+};
 class DataProcessing {
   constructor() {
     this.raw_user_data = null;
     this.formatted_user_data = null;
     this.clean_user_data = null;
+    this.user_emails = {};
   }
 
   load_CSV(filename) {
@@ -23,6 +30,8 @@ class DataProcessing {
 
     const rows = this.raw_user_data.split("\n");
     this.formatted_user_data = rows.map((row) => row.split(","));
+    // console.log(this.formatted_user_data[107]);
+    // console.log(this.formatted_user_data[106]);
     this.formatted_user_data.pop();
     this.formatted_user_data = this.formatted_user_data.map((user) => {
       const title_and_name = user[0].split(" ");
@@ -52,11 +61,13 @@ class DataProcessing {
   clean_data() {
     this.clean_user_data = this.formatted_user_data.map((user) => {
       let user_clean = user;
+      // 检查生日格式
       user_clean["date_of_birth"] = this.check_date(
         user_clean["date_of_birth"]
       );
+      // 检查年龄格式
       user_clean["age"] = this.check_age(user_clean["age"]);
-
+      // 检查邮箱
       switch (this.check_email(user_clean["email"])) {
         case 0: {
           user_clean[
@@ -75,6 +86,7 @@ class DataProcessing {
         }
       }
       const name = user_clean["email"].split("@")[0].split(".");
+      // 检查名字
       switch (
         this.check_name(user_clean["first_name"], user_clean["surname"])
       ) {
@@ -87,7 +99,29 @@ class DataProcessing {
           break;
         }
       }
+      // 检查年龄与生日
+      let age_date = this.calculate_age_date(
+        user_clean["age"],
+        user_clean["date_of_birth"]
+      );
+      user_clean["age"] = age_date[0];
+      user_clean["date_of_birth"] = age_date[1];
+      // 检查邮箱是否重复
+      // 存储邮箱
+      this.user_emails[user_clean["email"]] == undefined
+        ? (this.user_emails[user_clean["email"]] = [1, 0])
+        : this.user_emails[user_clean["email"]][0]++;
       return user_clean;
+    });
+    this.clean_user_data = this.clean_user_data.map((user) => {
+      if (this.user_emails[user["email"]][0] != 1) {
+        this.user_emails[user["email"]][1]++;
+        const parts = user["email"].split("@");
+        user["email"] =
+          parts[0] + (this.user_emails[user["email"]][1] + 1) + "@" + parts[2];
+        // console.log(this.user_emails[user["email"]][1]);
+      }
+      return user;
     });
   }
   // Check and rectify the date format
@@ -196,6 +230,32 @@ class DataProcessing {
   check_name(first_name, surname) {
     if (first_name == "" || surname == "") return 0;
     else return 1;
+  }
+  // 根据年龄计算日期
+  calculate_age_date(ageString, dateString) {
+    let newAgeString = this.calculate_age(dateString);
+    if (newAgeString == ageString) {
+      return [ageString, dateString];
+    } else if (newAgeString < 0) {
+      // 说明日期错误，需要返回正确日期
+      const today = new Date(date_convert(current_date));
+      let birth_year = today.getFullYear() - ageString;
+      let parts = dateString.split("/");
+      return [ageString, `${parts[0]}/${parts[1]}/${birth_year}`];
+    } else {
+      // 说明年龄错误，需要返回正确年龄
+      return [newAgeString, dateString];
+    }
+  }
+  calculate_age(dateString) {
+    const birthday = new Date(date_convert(dateString));
+    const today = new Date(date_convert(current_date));
+    let age = today.getFullYear() - birthday.getFullYear();
+    const m = today.getMonth() - birthday.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthday.getDate())) {
+      age--;
+    }
+    return age;
   }
 }
 
--- Code/ttt.js
+++ Code/ttt.js
@@ -1,58 +1,127 @@
-function formatDate(dateString) {
-    // 检查日期字符串的格式
-    if (/^\d{2}\/\d{2}\/\d{4}$/.test(dateString)) {
-      // 如果是DD/MM/YYYY格式，直接返回
-      return dateString;
-    } else if (/^\d{2}\/\d{2}\/\d{2}$/.test(dateString)) {
-      // 如果是DD/MM/YY格式，将YY转换为YYYY
-      const parts = dateString.split("/");
-      const year = parseInt(parts[2]) < 24 ? "20" + parts[2] : "19" + parts[2];
-      return parts[0] + "/" + parts[1] + "/" + year;
-    } else if (/^\d{4}\/\d{2}\/\d{1}$/.test(dateString)) {
-      // 如果是YYYY/MM/DD格式，将YYYY/MM/DD转换为DD/MM/YYYY
-      const parts = dateString.split("/");
-      return "0" + parts[2] + "/" + parts[1] + "/" + parts[0];
-    } else if (/^\d{2}-[a-zA-Z]{3}-\d{2}$/.test(dateString)) {
-      // 如果是DD-MMM-YY格式，将MMM转换为MM，YY转换为YYYY
-      const parts = dateString.split("-");
-      const months = {
-        Jan: "01",
-        Feb: "02",
-        Mar: "03",
-        Apr: "04",
-        May: "05",
-        Jun: "06",
-        Jul: "07",
-        Aug: "08",
-        Sep: "09",
-        Oct: "10",
-        Nov: "11",
-        Dec: "12",
-      };
-      const year = parseInt(parts[2]) < 24 ? "20" + parts[2] : "19" + parts[2];
-      return parts[0] + "/" + months[parts[1]] + "/" + year;
-    } else {
-      const parts = dateString.split(" ")
-      const months = {
-        January: "01",
-        February: "02",
-        March: "03",
-        April: "04",
-        May: "05",
-        June: "06",
-        July: "07",
-        August: "08",
-        Sepptember: "09",
-        October: "10",
-        November: "11",
-        December: "12",
-      };
-      return parts[0]+'/'+months[parts[1]]+'/'+parts[2];
-    }
-  }
-  
-  // 测试示例
-  const dateStrings = ["30/01/1943", "03/07/83", "2012/10/3", "19-Jun-98","08 April 1968"];
-  dateStrings.forEach((dateString) => {
-    console.log(dateString + " 格式化后为: " + formatDate(dateString));
-  });
+// const user1 = [
+//   "Mrs Hannah White",
+//   "08/06/1965",
+//   "62",
+//   "Hannah.White@example.com\r",
+// ];
+// const user2 = [
+//   "Mrs Hannah White",
+//   "02/11/2028",
+//   "21",
+//   "Hannah.White@example.com",
+// ];
+// const current_date = "26/02/2024";
+// function check_date(dateString) {
+//   // 检查日期字符串的格式
+//   if (/^\d{2}\/\d{2}\/\d{4}$/.test(dateString)) {
+//     // 如果是DD/MM/YYYY格式，直接返回
+//     return dateString;
+//   } else if (/^\d{2}\/\d{2}\/\d{2}$/.test(dateString)) {
+//     // 如果是DD/MM/YY格式，将YY转换为YYYY
+//     const parts = dateString.split("/");
+//     const year = parseInt(parts[2]) < 24 ? "20" + parts[2] : "19" + parts[2];
+//     return parts[0] + "/" + parts[1] + "/" + year;
+//   } else if (/^\d{4}\/\d{2}\/\d{1}$/.test(dateString)) {
+//     // 如果是YYYY/MM/DD格式，将YYYY/MM/DD转换为DD/MM/YYYY
+//     const parts = dateString.split("/");
+//     return "0" + parts[2] + "/" + parts[1] + "/" + parts[0];
+//   } else if (/^\d{2}-[a-zA-Z]{3}-\d{2}$/.test(dateString)) {
+//     // 如果是DD-MMM-YY格式，将MMM转换为MM，YY转换为YYYY
+//     const parts = dateString.split("-");
+//     const months = {
+//       Jan: "01",
+//       Feb: "02",
+//       Mar: "03",
+//       Apr: "04",
+//       May: "05",
+//       Jun: "06",
+//       Jul: "07",
+//       Aug: "08",
+//       Sep: "09",
+//       Oct: "10",
+//       Nov: "11",
+//       Dec: "12",
+//     };
+//     const year = parseInt(parts[2]) < 24 ? "20" + parts[2] : "19" + parts[2];
+//     return parts[0] + "/" + months[parts[1]] + "/" + year;
+//   } else {
+//     const parts = dateString.split(" ");
+//     const months = {
+//       January: "01",
+//       February: "02",
+//       March: "03",
+//       April: "04",
+//       May: "05",
+//       June: "06",
+//       July: "07",
+//       August: "08",
+//       Sepptember: "09",
+//       October: "10",
+//       November: "11",
+//       December: "12",
+//     };
+//     return parts[0] + "/" + months[parts[1]] + "/" + parts[2];
+//   }
+// }
+// function date_convert(date) {
+//   let parts = date.split("/");
+//   let newDate = parts[2] + "-" + parts[1] + "-" + parts[0];
+//   return newDate;
+// }
+
+// function calculate_age_date(age, date) {
+//   let ageByDate = calculate_age(date);
+//   if (ageByDate == age) {
+//     return [age, date];
+//   } else if (ageByDate < 0) {
+//     // 说明日期错误，需要返回正确日期
+//     const today = new Date(date_convert(current_date));
+//     let birth_year = today.getFullYear() - age;
+//     let parts = date.split("/");
+//     return [age, `${parts[0]}/${parts[1]}/${birth_year}`];
+//   } else {
+//     // 说明年龄错误，需要返回正确年龄
+//     return [ageByDate, date];
+//   }
+// }
+// function calculate_age(date) {
+//   const birthday = new Date(date_convert(date));
+//   const today = new Date(date_convert(current_date));
+//   let age = today.getFullYear() - birthday.getFullYear();
+//   const m = today.getMonth() - birthday.getMonth();
+//   if (m < 0 || (m === 0 && today.getDate() < birthday.getDate())) {
+//     age--;
+//   }
+//   return age;
+// }
+// let b = calculate_age_date(user1[2], user1[1]);
+// let d = calculate_age_date(user2[2], user2[1]);
+// console.log(b);
+// console.log(d);
+// const fs = require("fs");
+// const rows = [];
+// function load_CSV(filename) {
+//   try {
+//     this.raw_user_data = fs.readFileSync(filename + ".csv", "utf4");
+//     console.log("CSV data loaded successfully.");
+//   } catch (err) {
+//     console.error("Error loading CSV file:", err);
+//   }
+//   console.log(this.raw_user_data);
+// }
+
+// load_CSV("Data");
+
+user_emails = {};
+
+const emails = [
+  "example@example.com",
+  "test@test.com",
+  "example@example.com",
+  "test@test.com",
+  "another@example.com",
+];
+emails.forEach((e) => {
+  user_emails[e] == undefined ? (user_emails[e] = 1) : user_emails[e]++;
+});
+console.log(user_emails);
